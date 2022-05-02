3381548499d100310afd19a6945df0b5a39b7d6e
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-02 14:22:40 +0000

    Started styling in css

--- index.php
+++ index.php
@@ -18,19 +18,29 @@
     <title>Movie Ratings in PHP - Maria Goemans</title>
   </head>
   <body>
-    <?php echo "<h1>Age and Movie Ratings - in PHP!</h1>"; ?>
-    <?php
-      echo "<h3>Welcome!</h3>";
-      echo "<p>Welcome! This web page will take your age and verify what movies you are allowed to watch based on their ratings.</p>";
-      echo "<p>Please input your information below:</p>";
-    ?>
-    <?php echo "<h3>Your Age:</h3>"; ?>
-    <!-- Form for user input -->
-    <form action="./verify.php" method="post" target="results">
-      <label for="age">How old are you?</label>
-      <input type="number" step="1" min="1" name="user-age" placeholder="Your age..."><br><br>
-      <input type="submit" id="button" value="Check">
-    </form>
+    <!-- Div to limit text scrolling to boundaries -->
+    <div id="scroll-boundary">
+      <!-- Making text scroll across screen - learned from https://www.html.am/html-codes/marquees/html-scrolling-text.cfm -->
+      <marquee behavior="scroll" direction="left" scrollamount="10"><h1>Age and Movie Ratings - in PHP!</h1></marquee>
+    </div>
+    <!-- Div to put background image behind text -->
+    <div id="image-background">
+      <!-- Div to fix margins and align text with image -->
+      <div id="margin">
+        <?php
+          echo "<h3>Welcome!</h3>";
+          echo "<p>Welcome! This web page will take your age and verify what movies you are allowed to watch based on their ratings.</p>";
+          echo "<p>Please input your information below:</p>";
+        ?>
+        <?php echo "<h3>Your Age:</h3>"; ?>
+        <!-- Form for user input -->
+        <form action="./verify.php" method="post" target="results">
+          <label for="age">How old are you?</label>
+          <input type="number" step="1" min="1" name="user-age" placeholder="Your age..."><br><br>
+          <input type="submit" id="button" value="Check">
+        </form>
+      </div>
+    </div>
     <!-- Iframe for results -->
     <iframe id="results" name="results"></iframe>
   </body>
