e2d0168fb91967f8544023c112549d6498e9abea
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-03 14:36:36 +0000

    Added constants for thresholds to verify.php

--- verify.php
+++ verify.php
@@ -2,16 +2,20 @@
   //initializing and declaring variables
   $age = intval($_POST["user-age"]);
   $message = "";
+  //setting constants for age thresholds: first is 17, second is 13, third is 5
+  define(FIRST_THRESHOLD, 17);
+  define(SECOND_THRESHOLD, 13);
+  define(THIRD_THRESHOLD, 5);
   //first if statement: if age is greater than or equal to 17
-  if ($age >= 17) {
+  if ($age >= FIRST_THRESHOLD) {
     $message = "You are old enough to see an R-rated movie on your own.";
   }
   //otherwise, if age is greater than or equal to 13
-  else if ($age >= 13) {
+  else if ($age >= SECOND_THRESHOLD) {
     $message = "You can watch a PG-13 movie alone.";
   }
   //otherwise, if age is greater than or equal to 5
-  else if ($age >= 5) {
+  else if ($age >= THIRD_THRESHOLD) {
     $message = "You can see a G or PG movie on your own.";
   }
   //else (any age less than 5)
